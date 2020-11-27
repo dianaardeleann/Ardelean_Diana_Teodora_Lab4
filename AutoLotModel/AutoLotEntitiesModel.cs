d64b8544370d50f83f1c5e35a4d88ba0ec482ada
DIANA-TEODORA ARDELEAN <diana.teodora.ardelean@stud.ubbcluj.ro>
2020-11-27 16:02:42 +0200

    adaugat model

---
+++ AutoLotModel/AutoLotEntitiesModel.cs
@@ -0,0 +1,33 @@
+namespace AutoLotModel
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class AutoLotEntitiesModel : DbContext
+    {
+        public AutoLotEntitiesModel()
+            : base("name=AutoLotEntitiesModel")
+        {
+
+        } 
+
+        public virtual DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>()
+                .Property(e => e.FirstName)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Customer>()
+                .Property(e => e.LastName)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Customer>()
+                .Property(e => e.Contract_value)
+                .HasPrecision(7, 2);
+        }
+    }
+}
